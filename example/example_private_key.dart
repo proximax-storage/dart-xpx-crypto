8e95429ce1740d15dca6c94c0ccab3127ded00df
slackve <eleazartovar123@gmail.com>
2019-04-27 20:14:19 -0400

    update example

--- example/example_private_key.dart
+++ example/example_private_key.dart
@@ -1,19 +1,15 @@
-//import "package:nem2_crypto/nem2_crypto.dart";
-//
-//void main() {
-//  var pk = NewPublicKeyFromHexString(
-//      "1288C721FE54DF531DE53120612E76B127DD2C4C5E3E4AB6B90A4C1FEEAF1B75");
-//  var sk = NewPrivateKeyFromHexString(
-//      "0DA6F90F86835F4822D1C721DAF52C5E47AE1AD488D1733D42464CCC090413FC");
-//  var keyPair = NewKeyPair(sk, null);
-//  print(keyPair);
-//}
-
 import "package:nem2_crypto/nem2_crypto.dart";
 
 void main() {
-  String sk = "BB2B97D428832EFBA9816C62CC4911296EE3EE65DB19316D4AC1191028FE976C";
-  var d =  NewPrivateKeyFromHexString(sk);
-  var kp = NewKeyPair(d, null);
-  print(kp.toJson());
+
+  //  var sk = NewPrivateKeyFromHexString(
+  //      "BB2B97D428832EFBA9816C62CC4911296EE3EE65DB19316D4AC1191028FE976C");
+  //  var pk = NewPublicKeyFromHexString(
+  //      "36C2E79313AD12BCD736CD272216F598EA12BF76E334156846B35268DE9077F6");
+  //  var keyPair = NewKeyPair(sk, pk);
+  String sk =
+      "BB2B97D428832EFBA9816C62CC4911296EE3EE65DB19316D4AC1191028FE976C";
+  var d = NewPrivateKeyFromHexString(sk);
+  var keyPair = NewKeyPair(d, null);
+  print(keyPair.toJson());
 }
