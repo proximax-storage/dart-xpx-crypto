219ee9db5a2d4e71c63b4befd12a6e0d2ae06522
slackve <eleazartovar123@gmail.com>
2019-04-29 11:39:27 -0400

    new example

--- example/example_private_key.dart
+++ example/example_private_key.dart
@@ -1,27 +1,24 @@
 import "package:nem2_crypto/nem2_crypto.dart";
 import "dart:convert";
 import 'dart:typed_data';
+import 'package:hex/hex.dart';
+
 
 void main() {
-
-  //  var sk = NewPrivateKeyFromHexString(
-  //      "BB2B97D428832EFBA9816C62CC4911296EE3EE65DB19316D4AC1191028FE976C");
-  //  var pk = NewPublicKeyFromHexString(
-  //      "36C2E79313AD12BCD736CD272216F598EA12BF76E334156846B35268DE9077F6");
-  //  var keyPair = NewKeyPair(sk, pk);
   String sk =
       "BB2B97D428832EFBA9816C62CC4911296EE3EE65DB19316D4AC1191028FE976C";
   var d = NewPrivateKeyFromHexString(sk);
   var keyPair = NewKeyPair(d, null);
 
-  print(keyPair.toString());
+  print("${keyPair.toString()}\n");
 
   Uint8List msg = utf8.encode("test string");
 
   var sing = keyPair.sign(msg);
 
-  bool result = keyPair.verify(msg,  sing);
+  print("Signature: \"${HEX.encode(sing).toUpperCase()}\"\n");
 
-  print("verify: \"${result}\"");
+  bool result = keyPair.verify(msg,  sing);
+  print("Verify: \"${result}\"");
 
 }
